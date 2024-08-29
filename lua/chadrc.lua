3cf336b2e045e1d75c68f011c2692afdd71023e0
Dinno <tahadlrb7@gmail.com>
2024-08-29 21:14:13 +0330

    chore(chadrc):add float terminal configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -90,4 +90,16 @@ M.ui = {
   },
 }
 
+M.term = {
+  winopts = { number = false, relativenumber = false },
+  sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
+  float = {
+    relative = "editor",
+    row = 0.3,
+    col = 0.25,
+    width = 0.5,
+    height = 0.5,
+    border = "single",
+  },
+}
 return M
